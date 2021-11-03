91ae02a363ad9e0b146fef3c308849837bf45852
Nick Largent <nicklargent@gmail.com>
2021-11-03 16:42:57 -0400

    changes to support window shade (more steps)

--- BlindsController.ino
+++ BlindsController.ino
@@ -8,8 +8,13 @@ const int sensorMax = 1020;
 const int PHOTOCELL_PIN = A2;
 const int BUTTON1_PIN = 2;
 const int BUTTON2_PIN = 3;
+const int LED_PIN = LED_BUILTIN; //6;
 
 #define TRANSITION_LEVEL 10
+
+
+#define Sprint(a) //(Serial.print(a))
+#define Sprintln(a) //(Serial.println(a))
 
 // create an instance of the stepper class, specifying
 // the number of steps of the motor and the pins it's
@@ -19,35 +24,26 @@ bool blindsOpen = false;
 bool blindOverride = false;
 int trendCount = 0;
 
-int stepCount = 0;  // number of steps the motor has taken
+long stepCount = 0;  // number of steps the motor has taken
 bool stepCountSet = false;
 
 int mode = 1;
 
+
 void setup() {  
   Serial.begin(9600);
 
-  stepper.setSpeed(10);
+  stepper.setSpeed(15);
 
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON1_PIN, INPUT_PULLUP);
   pinMode(BUTTON2_PIN, INPUT_PULLUP);
-  
-  blink_ok();
-}
 
-
-void blink_ok() {
-  for (int i = 0; i<2; i++) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(5);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(5);    
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(5);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(100);
-  }
+  // test
+  //stepCount = 47320;
+  //blindsOpen = true;
+  //mode = 2;
 }
 
 void motorOff() {
@@ -58,16 +54,26 @@ void motorOff() {
 }
 
 void openBlinds() {
-  Serial.println("Opening Blinds...");
-  stepper.step(-stepCount);
+  Sprintln("Opening Blinds...");
+  long stepsRemaining = stepCount;
+  while (stepsRemaining > 0) {
+    int steps = STEPS_PER_REV / 100;
+    stepper.step(steps);
+    stepsRemaining -= steps;
+  }
   motorOff();
   blindsOpen = true;
   trendCount = 0;
 }
 
 void closeBlinds() {
-  Serial.println("Closing Blinds...");
-  stepper.step(stepCount);
+  Sprintln("Closing Blinds...");
+  long stepsRemaining = stepCount;
+  while (stepsRemaining > 0) {
+    int steps = STEPS_PER_REV / 100;
+    stepper.step(-steps);
+    stepsRemaining -= steps;
+  }  
   motorOff();
   blindsOpen = false;
   trendCount = 0;
@@ -83,10 +89,15 @@ void mode1_loop() {
   int button1 = digitalRead(BUTTON1_PIN);
   int button2 = digitalRead(BUTTON2_PIN);
 
-  Serial.print("MODE 1:  B1: ");
-  Serial.print(button1); Serial.print("   B2: ");
-  Serial.print(button2); Serial.print("   STEP_CT: ");
-  Serial.println(stepCount);
+  Sprint("MODE 1:  B1: ");
+  Sprint(button1); Sprint("   B2: ");
+  Sprint(button2); Sprint("   STEP_CT: ");
+  Sprintln(stepCount);
+
+  if (stepCountSet)
+    digitalWrite(LED_PIN, (millis() / 500) % 2);
+  else
+    digitalWrite(LED_PIN, (millis() / 100) % 2);
 
   // set the motor speed:
   if (button1 == LOW && button2 == HIGH) {
@@ -99,22 +110,21 @@ void mode1_loop() {
     motorOff();
 
     if (button1 == LOW && button2 == LOW) {
-      blink_ok();
       if (!stepCountSet) {
-        Serial.println("Step count reset");
+        Sprintln("Step count reset");
         stepCount = 0;
         stepCountSet = true;
         delay(2000);
-        blink_ok();
       } else {
-        Serial.println("Calibration complete.");
+        stepCount = abs(stepCount);
+        Sprintln("Calibration complete.");
         mode = 2;
+        digitalWrite(LED_PIN, 0);
         delay(500);
-        blink_ok();
       }
     }
     
-    delay(20);
+    delay(10);
   }
 }
 
@@ -125,21 +135,21 @@ void mode2_loop() {
   
   int analogValue = analogRead(PHOTOCELL_PIN);
   int value = map(analogValue, sensorMin, sensorMax, 0, 100);
-  Serial.print("MODE 2:  ");
+  Sprint("MODE 2:  ");
   if (blindsOpen)
-    Serial.print("OPEN  ");
+    Sprint("OPEN  ");
   else
-    Serial.print("CLOSED  ");
+    Sprint("CLOSED  ");
   if (blindOverride)
-    Serial.print(" (OVERRIDE) ");
-  Serial.print("  Light Level: "); Serial.print(analogValue);  Serial.print("  Adjusted: ");
-  Serial.print(value);  Serial.print("   Trend: ");
-  Serial.println(trendCount);
+    Sprint(" (OVERRIDE) ");
+  Sprint("  Light Level: "); Sprint(analogValue);  Sprint("  Adjusted: ");
+  Sprint(value);  Sprint("   Trend: ");
+  Sprintln(trendCount);
 
   if (button1 == LOW || button2 == LOW) {
     blindOverride = !blindOverride;
-    blink_ok();
-    delay(1000);
+    digitalWrite(LED_PIN, blindOverride);
+    
     if (blindsOpen)
       closeBlinds();
     else
